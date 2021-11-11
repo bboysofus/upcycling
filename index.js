320cd40434628e02b7f9c9f69412de8f305109e9
bboysofus <sofusba@gmail.com>
2021-11-11 21:24:30 +0100

    Added new page

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 let main = document.querySelector('main')
 let images = ['assets/1.jpg','assets/2.jpg','assets/3.jpg','assets/4.jpg']
 
-setInterval(function(){
-    let random = Math.floor(Math.random() * 4)
-    main.style.backgroundImage = "url(assets/2.jpg)"
-    main.style.backgroundImage = "url(assets/3.jpg)"
+const changeImage = () => {
+    if(main.style.backgroundImage == "url(assets/1.jpg)"){
+        main.style.backgroundImage = "url(assets/2.jpg)"
+    }
+    else if(main.style.backgroundImage == "url(assets/2.jpg)"){
+        main.style.backgroundImage = "url(assets/3.jpg)"
+    }
+    else if(main.style.backgroundImage == "url(assets/3.jpg)"){
+        main.style.backgroundImage = "url(assets/4.jpg)"
+    }
+    else(main.style.backgroundImage = "url(assets/1.jpg)")
+}
 
-},1000)
+setInterval(changeImage, 1000)
+
+// setInterval(function(){
+//     change()
+//     let random = Math.floor(Math.random() * 4)
+// },1000)
